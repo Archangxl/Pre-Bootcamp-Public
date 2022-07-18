2d6a554d938cafc70fe242f2bd1f3c0a07c707bb
Alexander Mueller <101312895+Archangxl@users.noreply.github.com>
2022-07-18 09:07:58 -0600

    Mueller_Alexander_JS_Tinkering

---
+++ it's.js
@@ -0,0 +1,8 @@
+var childHeight
+childHeight = 53
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+  If (childHeight>=52)
+  console.log('Get on that ride, kiddo!')
+  Else (childHeight<52)
+  console.log('Sorry Kiddo. Maybe next year')
+}
